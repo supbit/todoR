c8022e3fc5c776ca36859556b6b6be4ab9de197a
supbit <89527479+supbit@users.noreply.github.com>
2022-01-30 21:54:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
     return (
         <div className="App">
             <header>
-                <h1>Todos</h1>
+                <h1>Raghu ToDo Application</h1>
             </header>
             <Form
                 inputText={inputText}
